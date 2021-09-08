ef2d73dbf76e2a6a5932520e3e92f00d62e91b78
Thomas <thomasmckenna12gmail.com>
2021-09-07 20:53:04 -0400

    commit tree 954bf6d4fc2ae572ff4d04275e466ea9a8298484

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Your code here
+
+function createEmployeeRecord() {
+    let employee = {
+    firstName: 'Gray' , 
+    familyName: 'Worm',
+    title: 'Security',
+    payPerHour: 1,
+    timeInEvents: [],
+    timeOutEvents: []
+    }
+ 
+    return employee; 
+}
+
+function createEmployeeRecords(array1) {
+    let employeeRecords = [
+        ["moe", "sizlak", "barkeep", 2],
+        ["bartholomew", "simpson", "scamp", 3] 
+    ]
+    return employeeRecords;
+}
